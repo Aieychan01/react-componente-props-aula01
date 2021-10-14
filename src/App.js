3f1421747b09700aa007d71d263e80b95c38a713
Aieychan01 <87831895+Aieychan01@users.noreply.github.com>
2021-10-14 01:44:22 -0300

    envio da atividade

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Carta from './Carta'
+
+function App() {
+  return (
+    <div className="App">
+      <Carta remetente='Joana' destino='Minas Gerais'/>
+    </div>
+  );
+}
+
+export default App;
